91298ff63a7330d089a37db243969dad297cc9c9
DukeDingP <cumtdp0122@163.com>
2018-09-09 11:43:34 +0800

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -3,3 +3,19 @@ import("fmt")
 fuc main(){
  fmt.Print("hello world") 
 }
+
+//python
+//a=[1,2,3,4,5,6]
+//for i in range(len(a)):
+ 
+// print(i,a[i])
+
+fuc main(){
+ a:=[6]int{1,2,3,4,5,6}
+ for i,x:=range a{
+  fmt.Printf(i,x)
+ }
+ for j=0;j<len(a);j++{
+ fmt.Printf(a[j])
+ }
+}
